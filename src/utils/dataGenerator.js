2ba5e34d6d43c7fd3109563c0d5507a934f5e495
jeffreychun8 <jeffreychung2888@gmail.com>
2025-07-29 10:31:28 +0800

    Create dataGenerator.js

---
+++ src/utils/dataGenerator.js
@@ -0,0 +1,13 @@
+import { parameterCategories } from '../constants/parameters.js';
+
+export const generateIndustrialData = () => {
+    const data = [];
+    const waferRadius = 18;
+    const centerX = 20;
+    const centerY = 20;
+    
+    // All data generation logic here
+    // ... (moved from main component)
+    
+    return { headers, data };
+};
